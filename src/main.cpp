01b7ff3673d3b7766673aeebb46aef2a79e5826f
unknown <jbniyikiza20@gmail.com>
2025-05-10 21:19:42 +0200

    final firmware

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+
 BluetoothSerial SerialBT;
 LiquidCrystal_I2C lcd(0x27, 16, 2);  
 
@@ -16,14 +17,12 @@ bool lastButton2State;
 
 int brakeCount1 = 0;
 int brakeCount2 = 0;
-
 volatile int speedPulseCount = 0;
 int lastSpeed = 0;
 unsigned long lastSpeedCheck = 0;
 
 const char* deviceID = "ESP1";
 
-// Interrupt to count speed sensor pulses
 void IRAM_ATTR onSpeedSensor() {
   speedPulseCount++;
 }
@@ -34,15 +33,15 @@ void updateLCD() {
   lcd.print(brakeCount1);
   lcd.print(" B2:");
   lcd.print(brakeCount2);
-  lcd.print("  ");  // Padding to clear leftover chars
+  lcd.print("   ");  
 
   lcd.setCursor(0, 1);
   lcd.print("SPD:");
   lcd.print(lastSpeed);
-  lcd.print("     ");  // Padding
+  lcd.print("   ");  
 }
 
-// Send data in JSON format over Bluetooth and Serial
+
 void sendJsonData() {
   StaticJsonDocument<256> doc;
   doc["device"] = deviceID;
@@ -54,9 +53,9 @@ void sendJsonData() {
   String output;
   serializeJson(doc, output);
   SerialBT.println(output);
-  Serial.println(output); // Optional: log to USB serial
+  Serial.println(output);  
 
-  updateLCD(); 
+  updateLCD();
 }
 
 void setup() {
@@ -69,7 +68,7 @@ void setup() {
   lcd.setCursor(0, 0);
   lcd.print("Starting...");
 
-  pinMode(button1Pin, INPUT_PULLUP);   // will be changed under project testing.
+  pinMode(button1Pin, INPUT_PULLUP);
   pinMode(button2Pin, INPUT_PULLUP);
   pinMode(speedSensorPin, INPUT_PULLUP);
 
@@ -80,30 +79,29 @@ void setup() {
 
   delay(1000);
   lcd.clear();
-  updateLCD(); 
+  updateLCD();
 }
 
 void loop() {
-  // Check button 1
+  
   bool button1State = digitalRead(button1Pin);
+  bool button2State = digitalRead(button2Pin);
+
   if (button1State == LOW && lastButton1State == HIGH) {
     brakeCount1++;
     sendJsonData();
   }
   lastButton1State = button1State;
 
-  // Check button 2
-  bool button2State = digitalRead(button2Pin);
   if (button2State == LOW && lastButton2State == HIGH) {
     brakeCount2++;
     sendJsonData();
   }
   lastButton2State = button2State;
 
-  // Speed calculation every second
   unsigned long now = millis();
   if (now - lastSpeedCheck >= 1000) {
-    int currentSpeed = speedPulseCount; // Adjust unit as needed
+    int currentSpeed = speedPulseCount;
     if (currentSpeed != lastSpeed) {
       lastSpeed = currentSpeed;
       sendJsonData();
